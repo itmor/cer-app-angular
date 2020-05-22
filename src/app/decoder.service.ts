7a2ed8732543cd78ad339b95bde2b9260c3961d2
itmor <itmor.developer@gmail.com>
2020-05-22 23:26:31 +0300

    add decoder

--- src/app/decoder.service.ts
+++ src/app/decoder.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+declare const window: any;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DecoderService {
+  private reHex: RegExp = /^\s*(?:[0-9A-Fa-f][0-9A-Fa-f]\s*)+$/;
 
-  constructor() { }
+  decode(der: any, callBack: Function): void {
+    try {
+      const asn1: any = window.ASN1.decode(der);
+      if (asn1.typeName() !== 'SEQUENCE') {
+        const errorText =
+          'Неверная структура конверта сертификата (ожидается SEQUENCE)';
+        throw new Error(errorText);
+      } else {
+        callBack(asn1);
+      }
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  decodeBinaryString(str: any, callBack: Function) {
+    let der: any;
+
+    try {
+      if (this.reHex.test(str)) {
+        der = window.Hex.decode(str);
+      } else if (window.Base64.re.test(str)) {
+        der = window.Base64.unarmor(str);
+      } else {
+        der = str;
+        this.decode(der, callBack);
+      }
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  read(file: Blob, callBack: Function): void {
+    const reader: FileReader = new FileReader();
+    reader.onloadend = () => {
+      if (reader.error) {
+        throw new Error('Error reading');
+      } else {
+        this.decodeBinaryString(reader.result, callBack);
+      }
+    };
+
+    reader.readAsBinaryString(file);
+  }
 }
--- src/app/drop/drop.component.ts
+++ src/app/drop/drop.component.ts
@@ -1,5 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { LocalStorageService } from '../local-storage.service';
+import { DecoderService } from '../decoder.service';
 
 @Component({
   selector: 'drop',
@@ -8,8 +9,11 @@ import { LocalStorageService } from '../local-storage.service';
 })
 export class DropComponent {
   @Output() onDrop = new EventEmitter<boolean>();
+  private decoderService: DecoderService;
 
-  constructor(private localStorageService: LocalStorageService) {}
+  constructor(private localStorageService: LocalStorageService) {
+    this.decoderService = new DecoderService();
+  }
 
   public dragHandler(event: DragEvent): void {
     event.preventDefault();
@@ -17,6 +21,11 @@ export class DropComponent {
 
   public dropHandler(event: DragEvent): void {
     console.log(event.dataTransfer.files[0]);
+
+    this.decoderService.read(event.dataTransfer.files[0], (data: any) => {
+      console.log(data.sub[0]);
+    });
+
     this.localStorageService.addItem({
       name: 'we lol',
       id: 'ere',
