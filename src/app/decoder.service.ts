ea03e244e7361d4a4a4b6e2be9e1640f5116e4e6
itmor <itmor.developer@gmail.com>
2020-06-02 15:25:16 +0300

    delete vendor

--- src/app/decoder.service.ts
+++ src/app/decoder.service.ts
@@ -5,14 +5,56 @@ declare const window: any;
   providedIn: 'root',
 })
 export class DecoderService {
+  private reHex: RegExp = new RegExp(/^\s*(?:[0-9A-Fa-f][0-9A-Fa-f]\s*)+$/);
+
   public decode(file: Blob, callback: Function, errCallback: Function): void {
-    window.decodeAsn(
-      file,
-      (asn1Struct: any) => {
-        // console.log(asn1Struct);
-        callback(asn1Struct);
-      },
-      errCallback
-    );
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      if (reader.error) {
+        errCallback(
+          `Your browser couldn't read the specified file (error code ${reader.error.code}`
+        );
+      } else {
+        this.decodeBinaryString(<string>reader.result, callback, errCallback);
+      }
+    };
+    reader.readAsBinaryString(file);
   }
+
+  private decodeBinaryString(
+    str: string,
+    callBack: Function,
+    errCallback: Function
+  ): void {
+    let der: string;
+    try {
+      if (this.reHex.test(str)) {
+        der = window.Hex.decode(str);
+      } else if (window.Base64.re.test(str)) {
+        der = window.Base64.unarmor(str);
+      } else {
+        der = str;
+        this.getAsn(der, callBack, errCallback);
+      }
+    } catch (e) {
+      errCallback(e);
+    }
+  }
+
+  private getAsn(der: string, callback: Function, errCallback: Function): void {
+    try {
+      const asnStruct: any = window.ASN1.decode(der);
+      if (asnStruct.typeName() !== 'SEQUENCE') {
+        errCallback(
+          'Неверная структура конверта сертификата (ожидается SEQUENCE)'
+        );
+      }
+
+      this.parseFields(asnStruct, callback);
+    } catch (e) {
+      errCallback(e);
+    }
+  }
+
+  private parseFields(asnStruct: any, callback: Function): void {}
 }
